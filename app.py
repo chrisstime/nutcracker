738033fe911c9a34966f1227da537cba80463005
chrisstime <christine.vinaviles@gmail.com>
2019-03-06 14:04:55 +1100

    WIP: tk UI

--- app.py
+++ app.py
@@ -6,19 +6,34 @@ class Application(Frame):
     def say_hi(self):
         print("hi there, everyone!")
 
-    def create_widgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "Close Program"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
+    def exit_widget(self):
+        self.exit = Button(self)
+        self.exit["text"] = "Close Program"
+        self.exit["fg"] = "red"
+        self.exit["command"] = self.quit
 
-        self.QUIT.pack({"side": "left"})
+        self.exit.pack({"side": "right"})
 
-        self.generate_pass = Checkbutton(self)
-        self.generate_pass["text"] = "Generate Password",
+    def password_option_widget(self, option):
+        self.options = Checkbutton(self)
+        self.options["text"] = option
+
+        self.options.pack({"side": "top"}, padx=20, pady=20)
+
+    def generate_pass_widget(self):
+        self.generate_pass = Button(self)
+        self.generate_pass["text"] = 'Generate Password',
         self.generate_pass["command"] = generate_password('all_characters', password_length=25)
 
-        self.generate_pass.pack({"side": "left"}, padx=20)
+        self.generate_pass.pack({"side": "right"}, padx=20)
+
+    def create_widgets(self):
+        self.exit_widget()
+        self.generate_pass_widget()
+        self.password_option_widget(option='Letters only')
+        self.password_option_widget(option='Letters and numbers only')
+        self.password_option_widget(option='All characters (includes symbols)')
+
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -26,7 +41,7 @@ class Application(Frame):
         self.create_widgets()
 
 
-root = Tk()
+root = Tk(className='nutcracker')
 app = Application(master=root)
 app.mainloop()
 root.destroy()
