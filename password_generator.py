731cf11534b458491aef9992c6503502c1afa961
chrisstime <christine.vinaviles@gmail.com>
2019-03-07 11:26:39 +1100

    improve password generator, use dictionaries

--- password_generator.py
+++ password_generator.py
@@ -1,31 +1,30 @@
 # Password generator
 
 from random import sample, choices
-from string import ascii_letters, printable, digits
+from string import ascii_letters, punctuation, digits
 
-all_characters = printable
-letters_only = ascii_letters
-letters_and_numbers_only = letters_only + digits
-
-
-def split_characters(characters):
-    return [characters[i:i+1] for i in range(0, len(characters), 1)]
+options_dict = {
+    'symbols': punctuation,
+    'letters': ascii_letters,
+    'numbers': digits
+}
 
 
-def shuffle_characters(character_range):
-    if character_range == 'all_characters':
-        return sample(all_characters, len(all_characters))
-    elif character_range == 'letters_and_numbers_only':
-        return sample(letters_and_numbers_only, len(letters_and_numbers_only))
-    elif character_range == 'letters_only':
-        return sample(letters_only, len(letters_only))
+def shuffle_characters(symbols: bool, letters: bool, numbers: bool):
+    included_characters = list()
+    characters_dict = {'symbols': symbols, 'letters': letters, 'numbers': numbers}
+    for character_type, include_character in characters_dict.items():
+        if include_character:
+            included_characters.extend(options_dict[character_type])
+
+    return sample(included_characters, len(included_characters))
 
 
 def format_password(generated_password_array):
     return "".join(generated_password_array)
 
 
-def generate_password(character_range, password_length):
-    shuffled_characters = shuffle_characters(character_range)
+def generate_password(symbols: bool, letters: bool, numbers: bool, password_length):
+    shuffled_characters = shuffle_characters(symbols=symbols, letters=letters, numbers=numbers)
 
     return format_password(choices(shuffled_characters, k=password_length))
